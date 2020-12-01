02441a505783a55c3d08458d5861ba86979c7243
Davide <ciampad06@gmail.com>
2020-12-01 16:18:04 +0100

    Fine esercizio con ES5 e inizio con ES6

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Si scriva una funzione che accetti tre argomenti
+// un array e due numeri (a più piccolo di b). La
+// funzione ritornerà un nuovo array con i valori che
+// hanno la posizione compresa tra i due numeri
+//utilizzare ES5 e ES5.
+
+// ---------------ES5-----------------
+// $(document).ready(
+//   function(){
+//     var a = 2;
+//     var b = 5;
+//     var array = ["luca", "marco","davide","enrico","gianluca","salvatore"];
+//     console.log(array);
+//     var arrayTwo = newArray(a, b, array);
+//     console.log(arrayTwo);
+//   }
+// )
+//
+// function newArray(a, b, array){
+//   var newPosition=[];
+//   for(var i = a; i < b; i++){
+//     var c = array[i];
+//     newPosition.push(c);
+//   }
+//   return newPosition;
+// }
+
+
+// ---------------/ES5-----------------
+// ---------------ES6-----------------
+$(document).ready(
+  function(){
+    const a = 2;
+    const b = 5;
+    const array = ["luca", "marco","davide","enrico","gianluca","salvatore"];
+    console.log(array);
+  }
+)
+
+
+
+
+
+
+
+
+
+// ---------------/ES6-----------------
